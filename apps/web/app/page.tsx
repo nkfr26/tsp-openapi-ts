82076ff30e9b1e7e673ae3571935b30e70688a1d
nkfr26 <n@rfu.jp>
2025-07-22 00:54:56 +0900

    Hono

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,10 @@
-import { Button } from "@workspace/ui/components/button";
-
-export default function Page() {
+export default async function Page() {
+  const data = await fetch("http://127.0.0.1:8787");
+  const text = await data.text();
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <h1 className="text-2xl font-bold">{text}</h1>
       </div>
     </div>
   );

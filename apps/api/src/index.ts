f599b4a8fdd9ddf526c17f0421c7fd7e4f24a5ef
nkfr26 <n@rfu.jp>
2025-07-26 05:04:44 +0900

    openapi-typescript

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,24 @@
 import { Hono } from "hono";
+import { components, paths } from "@workspace/openapi-typescript/src/schema";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.use("/*", cors());
+
+app.get("/widgets", (c) => {
+  try {
+    return c.json<
+      paths["/widgets"]["get"]["responses"]["200"]["content"]["application/json"]
+    >({
+      items: [{ id: "string", weight: 0, color: "red" }],
+    });
+  } catch (err) {
+    return c.json<components["schemas"]["Error"]>({
+      code: 500,
+      message: "エラーが発生しました",
+    });
+  }
 });
 
 export default app;
